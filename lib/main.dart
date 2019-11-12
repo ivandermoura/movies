0ad477ac8f0f697a3fe7da3d5a383c81e6e88c66
ivandermoura <ivanderlp1@gmail.com>
2019-11-12 10:26:02 +0700

    add main2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hao/MovieList.dart';
-class Mainapp extends StatelessWidget {
+
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
